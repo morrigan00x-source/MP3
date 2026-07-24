63db52c0e94f854f3eebe01a77e236c29c9d97ee
shojo <morrigan@gmail.com>
2026-07-23 20:36:34 -0600

    en git solo server V0.1

--- mserver.py
+++ mserver.py
@@ -1,33 +1,65 @@
 import socket
 import threading
+import os 
+import queue
 
+#aqui llegan los mensajes
+cola_msg = queue.Queue()
 def clientthread(conn, addr):
+    #
+    #aqui enviara el tamaño en bytes del txt (tamaño txt), si es distinto al 
+    #de la interface enviara por bytes el archivo
+    #
     conn.send(bytes(f"Bienvenido {addr}", 'utf-8'))
     while True:
         try:
-            message = conn.recv(BUFFER_SIZE).decode('utf-8')
+            message = str(conn.recv(BUFFER_SIZE).decode('utf-8'))
+            #buscar comandos
             if message:
+                partesMsg = message.split(" ")
                 print(f"<{addr[0]}> {message}")
-                # Reenviamos el mensaje recibido a todos los demás clientes.
-                message_to_send = f"<{addr[0]}> {message}"
-                broadcast(message_to_send, conn)
+                
+                if(message.startswith("<descargar>")):
+                    None
+                elif(message.startswith("<last>")):
+                    msgToConsola(message)
+                elif(message.startswith("<next>")):
+                    msgToConsola(message)
+                elif(message.startswith("<stop>")):
+                    msgToConsola(message)
+                elif(message.startswith("<play>")):
+                    msgToConsola(message)
+                elif(message.startswith("<s>")): #valor del pot para el sonido valor de 0 a 100
+                    msgToConsola(message)
+                elif(message.startswith("<apodo>")):
+                    apodos_clients[partesMsg[1]] = conn 
+                
             else:
                 remove(conn)
         except:
             break
-
-def broadcast(message, connection):
+#manda el emnsaje a toda la red conectada
+def msgToConsola(message):
+    connection = apodos_clients["console"] 
     for clients in list_of_clients:
-        if clients != connection:
+        if clients == connection:
             try:
                 clients.send(bytes(message, 'utf-8'))
             except:
                 clients.close()
                 remove(clients)
-
+#quita direcciones usuarios
 def remove(connection):
     if connection in list_of_clients:
         list_of_clients.remove(connection)
+
+#esta funcion leera los mp3 de una carpeta y hara un txt con los names
+
+    
+
+#este hilo sera el reproductor mp3
+
+
 
 if __name__ == "__main__":
     host = '0.0.0.0'  # Esta función nos da el nombre de la máquina
@@ -39,6 +71,8 @@ if __name__ == "__main__":
     server.bind((host, port))
     server.listen(100)  # Escuchamos hasta 100 clientes
     list_of_clients = []  # Lista de clientes conectados
+    apodos_clients = {}
+
     print(f"Escuchando conexiones en: {(host, port)}")
     try:
         while True:
