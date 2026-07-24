3b5d54372986b7cb7ffbfc5dc50549d9a4483c55
shojo <morrigan@gmail.com>
2026-07-24 01:21:20 -0600

    modificacion de testeo apodos

--- mserver.py
+++ mserver.py
@@ -40,6 +40,10 @@ def clientthread(conn, addr):
             break
 #manda el emnsaje a toda la red conectada
 def msgToConsola(message):
+
+    print("apodos registrados: ")
+    for client in apodos_clients:
+        print(client, sep=" ")
     connection = apodos_clients["console"] 
     for clients in list_of_clients:
         if clients == connection:
